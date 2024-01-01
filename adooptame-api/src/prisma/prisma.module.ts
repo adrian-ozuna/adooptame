859c8d0f10a5de07f9af13d66ccc784c369b8637
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-01-01 00:53:25 -0400

    Set up prisma

--- adooptame-api/src/prisma/prisma.module.ts
+++ adooptame-api/src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { PrismaClient } from '@prisma/client';
 
 @Module({
+  imports: [PrismaClient],
   providers: [PrismaService]
 })
 export class PrismaModule {}
