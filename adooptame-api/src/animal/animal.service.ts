90d16322070afcfa244260a59fab2d22918e7c6c
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-01-01 23:53:16 -0400

    Animal creation

--- adooptame-api/src/animal/animal.service.ts
+++ adooptame-api/src/animal/animal.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AnimalService {}
+export class AnimalService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto) {
+    const animal = this.prisma.animal.create({
+      data: {
+        name: dto.name,
+        species: dto.species,
+        breed: dto.breed,
+        age: dto.age,
+        gender: dto.gender,
+        description: dto.description,
+        size: dto.size,
+        image_url: dto.image_url,
+        arrival_date: dto.arrival_date,
+        status: dto.status,
+        shelterId: dto.shelterId
+      }
+    })
+
+    return animal;
+  }
+}
