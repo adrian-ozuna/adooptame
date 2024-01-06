634391b97d37ba9596f8d2fa2795d73682ca4a93
Adrian Ozuna <adrian.gab.ozuna@outlook.com>
2024-01-06 19:45:23 -0400

    Added shelter crud operations

--- adooptame-api/src/shelter/shelter.service.ts
+++ adooptame-api/src/shelter/shelter.service.ts
@@ -1,23 +1,88 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ShelterService {
   constructor(private prisma: PrismaService) {}
-  async create(dto) {
-    const shelter = await this.prisma.shelter.create({
-      data: {
-        name: dto.name,
-        address: dto.name,
-        city: dto.city,
-        email: dto.email,
-        zipcode: dto.zipcode,
-        website: dto.website,
-        description: dto.description,
-        tel: dto.tel
+  async createShelter(dto) {
+    try {
+      const shelter = await this.prisma.shelter.create({
+        data: {
+          name: dto.name,
+          address: dto.name,
+          city: dto.city,
+          email: dto.email,
+          zipcode: dto.zipcode,
+          website: dto.website,
+          description: dto.description,
+          tel: dto.tel
+        }
+      })
+  
+      return shelter;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Shelter already exists.');
+        }
       }
-    })
+      throw e;
+    }
+
+  }
+
+  async getAllShelters() {
+    const shelters = await this.prisma.shelter.findMany();
+
+    if (!shelters) {
+      throw new NotFoundException('No shelters found.');
+    }
+
+    return shelters;
+  }
+
+  async getShelterById(id) {
+    const shelter = await this.prisma.shelter.findUnique({
+      where: {
+        id: id
+      }
+    });
+
+    if (!shelter) {
+      throw new NotFoundException('No shelter found.');
+    }
 
     return shelter;
+  }
+
+  async updateShelterById(id, dto) {
+    try {
+      const updateShelter = this.prisma.shelter.update({
+        where: {
+          id: id
+        },
+        data: dto
+      });
+  
+      return updateShelter;
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException('Shelter already exists.');
+        }
+      }
+      throw e;
+    }
+  }
+
+  async deleteShelter(id) {
+    const deleteShelter = this.prisma.shelter.delete({
+      where: {
+        id: id
+      }
+    });
+
+    return deleteShelter;
   }
 }
